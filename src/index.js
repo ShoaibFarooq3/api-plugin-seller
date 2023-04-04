09a870753d809e968d1a6678653d419079cbe2db
shoaibfarooq3 <shoaib.farooq@codistan.org>
2023-04-04 10:31:43 +0500

    New

--- src/index.js
+++ src/index.js
@@ -73,15 +73,15 @@ const resolvers = {
   },
   Query: {
     async getAllSeller(parent, args, context, info) {
-      console.log(args)
-      console.log(context.collections);
+      // console.log(args)
+      // console.log(context.collections);
       if (context.user === undefined || context.user === null) {
         throw new Error("Unauthorized access. Please login first");
       }
       const { Accounts } = context.collections;
       const allUsersResponse = await Accounts.find({ roles: "vendor" }).toArray();
       console.log(allUsersResponse)
-      return allUsersResponse;
+      // return allUsersResponse;
     }
   },
   Mutation: {
